79583d85fc3f68d91ededbbec0f5a47262529465
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-05-01 11:45:19 +0300

    HomeScreen Finish Layout
    - add bestSeller flatlist with dummy data.
    - add search flatlist with dummy data.
    - add BestSellerCard Component.
    - add BookCard Component.

--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -1,29 +1,48 @@
-import { ImageBackground, StyleSheet, Text, View , Image , TextInput} from "react-native";
+import { ImageBackground, StyleSheet, Text, View , Image , TextInput, StatusBar} from "react-native";
 import React, { useState } from "react";
 import COLORS from "@/constants/colors";
 
 const HomeHeader = () => {
 
     const [search , setSearch] = useState('');
+    const [counter , setCounter] = useState(0);
 
     return (
         <View style={styles.container}>
+            <StatusBar barStyle="light-content"/>
             <ImageBackground 
                 source={{uri: 'https://s3-alpha-sig.figma.com/img/a8d4/ce20/9979d5c6741421e4f3b2ad4ecf0dbd2f?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BhXWQU-hLVpQOg5GAlVU~v0WrtY-d82I~9sLuyOO8nbTDs22vdn49zugBNaZGqyxr~Z-EfJ-yuaNVXZGI0-y73yAPLDftHx7~WhLrFa0iRJx2-VH1ku-TlsX~X0tGdnlODY311VoFZ4Q1Ye5J6PVCVL~cP~Jw5L1laQUdYA7342zPym2D1RbVeHj~qHpes6yNmwd-f4RQB6CR9niBYgPdFY1TncdoNaYK7HX9oQLiW-cP2m41fyh0aP8giaBDEk-Jynaj8DgV8G0TiTHknq9Q9Djl6Td2LzgQY3kbYzTzpVkQsnld1uapTd4b600PpGg-3zCAZFALydI90cINq8-pg__'}}
                 style={{height: 200}}
-                imageStyle={{ borderBottomLeftRadius: 50 , borderBottomRightRadius: 50 }}
+                imageStyle={{ borderBottomLeftRadius: 50 , borderBottomRightRadius: 50 , opacity: 0.87}}
             >
                 <View style={styles.header}>
                     <Text style={styles.homeText}>Home</Text>
                     <View style={styles.headerIcons}>
                         <View>
-                            <Image source={require('../assets/images/icons/logout.png')}/>
+                            <Image 
+                                source={require('../assets/images/icons/logout.png')}
+                                // style={{width: 25 , height: 25}}
+                            />
+                        </View>
+                        <View style={styles.cartBox}>
+                            <ImageBackground 
+                                source={require('../assets/images/icons/cart.png')}
+                                style={{
+                                    width: 35.71, 
+                                    height: 31.75,
+                                }}
+                                
+                            >
+                                <View style={styles.counter}>
+                                    <Text style={styles.counterText}>{counter}</Text>
+                                </View>
+                            </ImageBackground>
                         </View>
                         <View>
-                            <Image source={require('../assets/images/icons/cart.png')}/>
-                        </View>
-                        <View>
-                            <Image source={require('../assets/images/icons/profile.png')}/>
+                            <Image 
+                                source={require('../assets/images/icons/profile.png')}
+                                style={{width: 40 , height: 40}}
+                            />
                         </View>
                     </View>
                 </View>
@@ -49,17 +68,14 @@ export default HomeHeader;
 
 const styles = StyleSheet.create({
     container: {
-        // borderWidth: 1
+        backgroundColor: COLORS.secondary,
     },
     header: {
-        marginTop: 20,
+        marginTop: '10%',
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginHorizontal: '5%',
-        // marginVertical: '10%',
-        borderWidth: 1,
-        borderColor: 'green',
     },
     homeText: {
         color: COLORS.secondary,
@@ -69,9 +85,7 @@ const styles = StyleSheet.create({
     headerIcons: {
         flexDirection: 'row',
         alignItems: 'center',
-        borderWidth: 1,
-        borderColor: 'green',
-        width: '35%',
+        width: 150,
         justifyContent: 'space-between'
     },
     usernameText: {
@@ -79,19 +93,49 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         fontSize: 16,
         marginHorizontal: '5%',
-        marginVertical: '3%',
-        borderWidth: 1,
-        borderColor: 'green',
+        marginTop: '5%'
     },
     searchBar: {
-        borderWidth: 1,
-        borderColor: 'green',
         backgroundColor: COLORS.secondary,
+        width: '89%',
+        paddingLeft: '5%',
+        borderBottomLeftRadius: 5,
+        borderTopLeftRadius: 5,
     },
     searchBox: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginHorizontal: '10%',
+        justifyContent: 'space-between',
+        padding: 3,
+        backgroundColor: COLORS.secondary,
+        borderRadius: 5,
+        marginVertical: '5%'
     },
     searchIconBox: {
-        backgroundColor: COLORS.secondary
+        backgroundColor: COLORS.secondary,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '3%',
+        borderBottomRightRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    counter: {
+        width: 21.43,
+        height: 21.43,
+        backgroundColor: COLORS.primary,
+        alignItems: 'center',
+        borderRadius: 50,
+        position: 'absolute',
+        right: 25,
+        bottom: 20
+    },
+    counterText: {
+        color: COLORS.white,
+        fontSize: 16,
+        fontWeight: '700'
+    },
+    cartBox: {
+        borderWdith: 1,
+        borderColor: 'green'
     }
 });
