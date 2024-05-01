666bc17ab7cae55a5f58e9cd523176f09caab435
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-05-01 12:20:50 +0300

    merge the rest branchs onto branch app/profileScreen

--- constants/colors.js
+++ constants/colors.js
@@ -1,17 +1,17 @@
 const COLORS = {
-  primary: '#29648F',         // Primary color for buttons, headers, etc.
-  primary_70: '#00B295',         // Primary color for buttons, headers, etc.
-  secondary: '#EEEEEE',       // Secondary color for less important elements
-  background: '#EEEEEE',      // Background color for screens
-  text: '#333333',            // Text color for body text
-  placeholderText: '#999999', // Text color for placeholder text
-  border: '#CED4DA',          // Border color for input fields, etc.
-  success: '#28a745',         // Success color for notifications, etc.
-  danger: '#dc3545',          // Danger color for errors, warnings, etc.
-  warning: '#ffc107',         // Warning color for alerts, etc.
-  info: '#17a2b8',            // Info color for informational messages, etc.
-  white: '#FFFFFF',            // White color
-  black: '#000000',           // Black color
+  primary: '#29648F',             // Primary color for buttons, headers, etc.
+  primary_70: '#6993B1',          // Primary color for buttons, headers, etc.
+  secondary: '#EEEEEE',           // Secondary color for less important elements
+  background: '#EEEEEE',          // Background color for screens
+  text: '#333333',                // Text color for body text
+  placeholderText: '#999999',     // Text color for placeholder text
+  border: '#CED4DA',              // Border color for input fields, etc.
+  success: '#28a745',             // Success color for notifications, etc.
+  danger: '#dc3545',              // Danger color for errors, warnings, etc.
+  warning: '#ffc107',             // Warning color for alerts, etc.
+  info: '#17a2b8',                // Info color for informational messages, etc.
+  white: '#FFFFFF',               // White color
+  black: '#000000',               // Black color
 };
 
 export default COLORS;
---
+++ screens/ProfileScreen.jsx
@@ -0,0 +1,15 @@
+import {View , Text, StyleSheet} from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text>Hello from profile screen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+
+});
