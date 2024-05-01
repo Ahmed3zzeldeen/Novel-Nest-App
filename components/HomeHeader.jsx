5a40aee1cc98c83401a385ce72b4b5eb9f74a0b8
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-05-01 11:45:19 +0300

    HomeScreen Finish Layout
    - add bestSeller flatlist with dummy data.
    - add search flatlist with dummy data.
    - add BestSellerCard Component.
    - add BookCard Component.

--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -1,13 +1,15 @@
-import { ImageBackground, StyleSheet, Text, View , Image , TextInput} from "react-native";
+import { ImageBackground, StyleSheet, Text, View , Image , TextInput, StatusBar} from "react-native";
 import React, { useState } from "react";
 import COLORS from "@/constants/colors";
 
 const HomeHeader = () => {
 
     const [search , setSearch] = useState('');
+    const [counter , setCounter] = useState(0);
 
     return (
         <View style={styles.container}>
+            <StatusBar barStyle="light-content"/>
             <ImageBackground 
                 source={{uri: 'https://s3-alpha-sig.figma.com/img/a8d4/ce20/9979d5c6741421e4f3b2ad4ecf0dbd2f?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BhXWQU-hLVpQOg5GAlVU~v0WrtY-d82I~9sLuyOO8nbTDs22vdn49zugBNaZGqyxr~Z-EfJ-yuaNVXZGI0-y73yAPLDftHx7~WhLrFa0iRJx2-VH1ku-TlsX~X0tGdnlODY311VoFZ4Q1Ye5J6PVCVL~cP~Jw5L1laQUdYA7342zPym2D1RbVeHj~qHpes6yNmwd-f4RQB6CR9niBYgPdFY1TncdoNaYK7HX9oQLiW-cP2m41fyh0aP8giaBDEk-Jynaj8DgV8G0TiTHknq9Q9Djl6Td2LzgQY3kbYzTzpVkQsnld1uapTd4b600PpGg-3zCAZFALydI90cINq8-pg__'}}
                 style={{height: 200}}
@@ -23,7 +25,10 @@ const HomeHeader = () => {
                             <Image source={require('../assets/images/icons/cart.png')}/>
                         </View>
                         <View>
-                            <Image source={require('../assets/images/icons/profile.png')}/>
+                            <Image 
+                                source={require('../assets/images/icons/profile.png')}
+                                style={{width: 40 , height: 40}}
+                            />
                         </View>
                         <View style={styles.cartBox}>
                             <ImageBackground 
@@ -99,9 +104,19 @@ const styles = StyleSheet.create({
     },
     searchBar: {
         backgroundColor: COLORS.secondary,
+        width: '89%',
+        paddingLeft: '5%',
+        borderBottomLeftRadius: 5,
+        borderTopLeftRadius: 5,
     },
     searchBox: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginHorizontal: '10%',
+        justifyContent: 'space-between',
+        padding: 3,
+        backgroundColor: COLORS.secondary,
+        borderRadius: 5,
+        marginVertical: '5%'
     },
     searchIconBox: {
         backgroundColor: COLORS.secondary,
