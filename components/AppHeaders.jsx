3210041e191d466ce5498cb9c6223b03c6045dd2
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2024-05-07 02:17:54 +0300

    Refactor route names in AppHeaders and DashboardScreen
    - Update route name in AppHeaders from ROUTES.DASHBOARD.MANAGE_USERS to ROUTES.DASHBOARD.LIST_OF_USERS.
    - Update route name in DashboardScreen from ROUTES.DASHBOARD.MANAGE_USERS to ROUTES.DASHBOARD.LIST_OF_USERS.

--- components/AppHeaders.jsx
+++ components/AppHeaders.jsx
@@ -89,7 +89,7 @@ const AppHeaders = () => {
         options={{ headerTitle: "Reset password", presentation: "modal" }}
       />
       <Stack.Screen
-        name={ROUTES.DASHBOARD.MANAGE_USERS}
+        name={ROUTES.DASHBOARD.LIST_OF_USERS}
         options={{
           headerTitle: "Users",
           headerTitleAlign: "center",
