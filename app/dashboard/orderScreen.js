2c9bdd7cb0306d1f4034bd4c033cf8165dff66e2
moustafahany <moustafahany@gmail.com>
2024-05-06 20:13:01 +0300

    Finish ListOfOrdersScreen and OrderScreen
    - add list-of-orders route.
    - add OrderScreen and ListOfOrdersScreen.
    - modify AppHeaders.

---
+++ app/dashboard/orderScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import OrderScreen from '../../screens/Dashboard/OrderScreen';
+
+
+const orderScreen = () => {
+  return <OrderScreen />;
+};
+
+export default orderScreen;
