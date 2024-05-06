3bb8d14066a491a7278585f9558e09d7070fe4bf
MoustafaHany <152873506+MoustafaHany@users.noreply.github.com>
2024-05-07 02:29:09 +0300

    Finish ListOfOrdersScreen and OrderScreen (#20)
    * Finish ListOfOrdersScreen and OrderScreen
    
    - add list-of-orders route.
    - add OrderScreen and ListOfOrdersScreen.
    - modify AppHeaders.
    
    * Remove unused routes in constants/routes.js
    
    * Refactor route names in AppHeaders and DashboardScreen
    
    - Update route name in AppHeaders from ROUTES.DASHBOARD.MANAGE_USERS to ROUTES.DASHBOARD.LIST_OF_USERS.
    - Update route name in DashboardScreen from ROUTES.DASHBOARD.MANAGE_USERS to ROUTES.DASHBOARD.LIST_OF_USERS.
    
    * Rename OrderScreen to AddNewOrderScreen and update ListOfOrdersScreen
    
    ---------
    
    Co-authored-by: moustafahany <moustafahany@gmail.com>
    Co-authored-by: Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>

--- components/AppHeaders.jsx
+++ components/AppHeaders.jsx
@@ -89,7 +89,7 @@ const AppHeaders = () => {
         options={{ headerTitle: "Reset password", presentation: "modal" }}
       />
       <Stack.Screen
-        name={ROUTES.DASHBOARD.MANAGE_USERS}
+        name={ROUTES.DASHBOARD.LIST_OF_USERS}
         options={{
           headerTitle: "Users",
           headerTitleAlign: "center",
@@ -99,6 +99,22 @@ const AppHeaders = () => {
       <Stack.Screen
         name={ROUTES.DASHBOARD.LIST_OF_BOOKS}
         options={{headerTitle: "List of books"}}
+      />
+      <Stack.Screen
+        name={ROUTES.DASHBOARD.ADD_NEW_ORDER}
+        options={{
+          headerTitle: "Order Screen",
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: "#29648f" },
+        }}
+      />
+      <Stack.Screen
+        name={ROUTES.DASHBOARD.LIST_OF_ORDERS}
+        options={{
+          headerTitle: "List Of Orders",
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: "#29648f" },
+        }}
       />
     </Stack>
   );
