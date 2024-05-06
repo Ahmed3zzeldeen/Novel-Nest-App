271c04636619c29dd4165b542425203b0da32026
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2024-05-07 02:26:50 +0300

    Rename OrderScreen to AddNewOrderScreen and update ListOfOrdersScreen

--- app/dashboard/orderScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import OrderScreen from '../../screens/Dashboard/OrderScreen';
-
-
-const orderScreen = () => {
-  return <OrderScreen />;
-};
-
-export default orderScreen;
