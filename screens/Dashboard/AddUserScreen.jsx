95624902ba9c4e53f297af66b058a972f84964aa
philosan <philopaterwaheed9@gmail.com>
2024-05-07 12:38:59 -0400

    fixed Inputs
    Added border raduis to the profile picture

--- screens/Dashboard/AddUserScreen.jsx
+++ screens/Dashboard/AddUserScreen.jsx
@@ -2,59 +2,28 @@ import {
   Pressable,
   StyleSheet,
   View,
-  TextInput,
   Text,
   FlatList,
-  Button,
   Image,
 } from "react-native";
 import { React, useEffect, useState } from "react";
-import { FontAwesome5, FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 import ROUTES from "../../constants/routes";
 import { router } from "expo-router";
 import * as ImagePicker from 'expo-image-picker';
 import { SafeAreaView } from "react-native-safe-area-context";
-import {uplouadFile, getLink} from "../../firebase/apis/storage";
+import { uplouadFile, getLink } from "../../firebase/apis/storage";
 import COLORS from "@/constants/colors";
 import profilePic from "../../assets/images/icons/iconPlaceHolder.png"
-import { Switch } from "react-native-web";
+import { CustomTextInput } from "@/components";
 const AddUserScreen = () => {
-  const [userNameprev, setUserNameprev] = useState();
-  const [firstNameprev, setFirstNameprev] = useState();
-  const [lastNameprev, setLastNameprev] = useState();
-  const [emailprev, setEmailprev] = useState();
-  const [userNameinput, setUserNameinput] = useState();
-  const [firstNameinput, setFirstNameinput] = useState();
-  const [lastNameinput, setLastNameinput] = useState();
-  const [emailinput, setEmailinput] = useState();
-  const [passinput, setPassinput] = useState();
+  const [usernameInput, setUsernameInput] = useState();
+  const [firstNameInput, setFirstNameInput] = useState();
+  const [lastNameInput, setLastNameInput] = useState();
+  const [emailInput, setEmailInput] = useState();
+  const [passInput, setPassInput] = useState();
   const [image, setImage] = useState(null);
   //require users from the database
-
-  const onChange= (type, text) => {
-      switch (type) {
-   	case 1:
-		setFirstNameinput(text);
-		setFirstNameprev(text);
-		break;
-     	case 2:
-		setLastNameinput(text);
-	setLastNameprev(text);
-	break;
-  	case 3:
-	setUserNameinput(text);
-	setUserNameprev(text);
-	break;
-  	case 4:
-	setEmailinput(text);
-	setEmailprev(text);
-	break;
-  	case 5:
-	setPassinput(text);
-		break;
-      		break;
-      }
-  };
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -63,6 +32,7 @@ const AddUserScreen = () => {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+  
     });
 
     console.log(result);
@@ -72,71 +42,59 @@ const AddUserScreen = () => {
     }
   };
   const uploadImage = async () => {
-       const response = await fetch(image);
-       const blob = await response.blob();
-       const ref = await uplouadFile ('bsbs',blob);
-       return (await getLink(ref.ref));
+    const response = await fetch(image);
+    const blob = await response.blob();
+    const ref = await uplouadFile(`avatar-${new Date()}`, blob);
+    return (await getLink(ref.ref));
   };
-  const Input = ({ text,placeHolder, width , type}) => {
-    return (
-      <View style={{flex :1 , gap : 5}}>
-	<Text style = {{width : 160 , height : 20 , fontWeight:"700" , color: COLORS.primary,fontFamily: "Fira Sans",}}>{text}</Text>
-	<TextInput placeholder = {placeHolder}
-        placeholderTextColor={COLORS.primary_70}
-        onChangeText={(text) => onChange(type,text)}
-	style = {{width : width , backgroundColor: COLORS.secondary , height : 30 , fontFamily: "Fira Sans",borderTopColor:COLORS.primary,borderRadius: 5}}
-	>
-	</TextInput>
-      </View>
-    );
-  };
+
   return (
     <SafeAreaView style={styles.screenContainer}>
       <View style={styles.buttonsArea}>
-	  <Pressable
-	    style={{ alignSelf: "flex-end",margin: 10 }}
-	    onPress={() => {
-	      router.replace(ROUTES.AUTH.SIGN_OUT);
-	    }}
-	  >
-	    <Text style={{ color: COLORS.primary, fontSize: 20 }}>
-	      <FontAwesome6
-		name="door-open"
-		size={24}
-		color={COLORS.primary}
-		style={{ margin: 10 }}
-	      />
-	      logout
-	    </Text>
-	  </Pressable>
+        <Pressable
+          style={{ alignSelf: "flex-end", margin: 10 }}
+          onPress={() => {
+            router.replace(ROUTES.AUTH.SIGN_OUT);
+          }}
+        >
+          <Text style={{ color: COLORS.primary, fontSize: 20 }}>
+            <FontAwesome6
+              name="door-open"
+              size={24}
+              color={COLORS.primary}
+              style={{ margin: 10 }}
+            />
+            logout
+          </Text>
+        </Pressable>
       </View>
       <Text style={styles.text}>User Preview</Text>
-      <View style = {styles.AvatarArea}>
-	  <Image source={(image)? { uri: image }: profilePic} style={{ width: 50, height: 50 , borderRadius:5 }} />
-	  <Pressable style = {styles.avatarButton} onPress = {()=>{pickImage();}}>
-	      <Text style= {{color:COLORS.secondary,fontSize :18, fontFamily: "Fira Sans" , fontWeight:"700" , alignSelf:"center" }}>
-		  Avatar
-	      </Text> 
-	      <FontAwesome6
-		name="cloud-arrow-up"
-		size={18}
-		color={COLORS.secondary}
-		style={{ alignSelf : "center" }}
-	      />
-	  </Pressable>
+      <View style={styles.AvatarArea}>
+        <Image source={(image) ? { uri: image } : profilePic} style={{ width: 50, height: 50, borderRadius: 100 }} />
+        <Pressable style={styles.avatarButton} onPress={() => { pickImage(); }}>
+          <Text style={{ color: COLORS.secondary, fontSize: 18, fontFamily: "Fira Sans", fontWeight: "700", alignSelf: "center" }}>
+            Avatar
+          </Text>
+          <FontAwesome6
+            name="cloud-arrow-up"
+            size={18}
+            color={COLORS.secondary}
+            style={{ alignSelf: "center" }}
+          />
+        </Pressable>
       </View>
-      <Text style= {styles.Text}>Username: {userNameprev} </Text>
-      <Text style= {styles.Text}>First Name: {firstNameprev} </Text>
-      <Text style= {styles.Text}>Last Name: {lastNameprev} </Text>
-      <Text style= {styles.Text}>Email: {emailprev} </Text>
-      <View style ={styles.inputsArea}>
-	    <View style = {styles.firstRow}>
-		<Input text = "First Name:" placeHolder = "Robert"  width = {160} type ={1}  />
-		<Input text = "Last Name:" placeHolder = "martin"  width = {160}type ={2}/>
-	    </View>
-	    <Input text = "Username:" placeHolder = "RobertMartin123"  width = {"100%"}type ={3}/>
-	    <Input text = "Email:" placeHolder = "example@something.com"  width = {"100%"}type ={4}/>
-	    <Input text = "Password" placeHolder = "Password here!"  width = {"100%"}type ={6}/>
+      <Text style={styles.Text}>Username: {usernameInput} </Text>
+      <Text style={styles.Text}>First Name: {firstNameInput} </Text>
+      <Text style={styles.Text}>Last Name: {lastNameInput} </Text>
+      <Text style={styles.Text}>Email: {emailInput} </Text>
+      <View style={styles.inputsArea}>
+        <View style={styles.firstRow}>
+          <CustomTextInput label="First Name:" placeholder="Robert" value={firstNameInput} onChangeText={(e) => setFirstNameInput(e)} />
+          <CustomTextInput label="Last Name:" placeholder="martin" value={lastNameInput} onChangeText={(e) => setLastNameInput(e)} />
+        </View>
+        <CustomTextInput label="Username:" placeholder="RobertMartin123" value={usernameInput} onChangeText={(e) => setUsernameInput(e)} />
+        <CustomTextInput label="Email:" placeholder="example@something.com" value={emailInput} onChangeText={(e) => setEmailInput(e)} />
+        <CustomTextInput secureTextEntry={true} label="Password" placeholder="Password here!" value={passInput} onChangeText={(e) => setPassInput(e)} />
       </View>
     </SafeAreaView>
   );
@@ -148,39 +106,38 @@ const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
     backgroundColor: COLORS.white,
-    paddingLeft : 10 ,
+    paddingLeft: 10,
   },
-  avatarButton:{
+  avatarButton: {
     flex: 1,
     backgroundColor: COLORS.primary,
-    width : 113, 
-    height : 34 , 
-    borderRadius: 5, 
-    justifyContent : "center",
-    alignSelf:"center",
-    gap: 3, 
+    width: 113,
+    height: 34,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignSelf: "center",
+    gap: 3,
     flexDirection: "row",
-    
+
   },
-  AvatarArea:{
-      flex :1 , 
-      flexDirection: "row",
-      width: 171, 
-      maxHeight: 50 , 
-      gap: 8 , 
-      marginTop: 10, 
-      marginBottom : 10 ,
-    },
-  firstRow:{
-      flex : 1 ,
-      flexDirection: "row",
-      flexWrap : "wrap",
-      gap : 10,
-      rowGap : 30,
+  AvatarArea: {
+    flex: 1,
+    flexDirection: "row",
+    width: 171,
+    maxHeight: 50,
+    gap: 8,
+    marginTop: 10,
+    marginBottom: 10,
   },
-  inputsArea:{
-      gap : 10,
-      marginRight : 30,
+  firstRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    gap: 10
+  },
+  inputsArea: {
+    gap: 10,
+    marginRight: 30,
   },
   text: {
     color: COLORS.primary,
@@ -207,6 +164,6 @@ const styles = StyleSheet.create({
     fontFamily: "Fira Sans",
     fontSize: 16,
     fontWeight: "700",
-      marginBottom : 10 ,
+    marginBottom: 10,
   }
 });
