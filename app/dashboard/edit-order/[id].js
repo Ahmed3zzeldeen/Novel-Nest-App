a0d2a66527a1d16e1aa8bb2db31d80270a3808d1
moustafahany <moustafahany@gmail.com>
2024-05-09 22:05:28 +0300

    made orders screen

---
+++ app/dashboard/edit-order/[id].js
@@ -0,0 +1,35 @@
+import ROUTES from "@/constants/routes";
+import { BookScreen } from "@/screens";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Pressable,
+  Text,
+} from "react-native";
+import EditOrderScreen from "@/screens/Dashboard/EditOrderScreen";
+
+export default function Page() {
+  const router = useRouter();
+  const { id, number } = useLocalSearchParams();
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: () => {
+            return (
+              <Text style={{ fontSize: 20, color: "#fff", fontWeight: "bold" }}>
+                Order {number}
+              </Text>
+            );
+          },
+          presentation: "modal",
+        }}
+      />
+      <EditOrderScreen orderId={id} number={number} />
+      
+    </>
+  );
+}
